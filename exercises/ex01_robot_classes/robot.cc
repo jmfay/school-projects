e877aaf8cd377be79ad8af8e2ae8faf3eb47160c
fayxx092 <fayxx092@umn.edu>
2019-09-12 13:09:41 -0500

    Finish robot exercises

--- exercises/ex01_robot_classes/robot.cc
+++ exercises/ex01_robot_classes/robot.cc
@@ -3,6 +3,27 @@
 
 #include "robot.h"
 
+Robot::Robot(int x, int y, float theta) {
+  position_[0] = x;
+  position_[1] = y;
+  direction_angle_ = theta;
+}
+
+
+void Robot::set_position( int x, int y) {
+  position_[0] = x;
+  position_[1] = y;
+}
+int* Robot::get_position() {
+  return position_;
+}
+void Robot::set_direction_angle_rad(float d) {
+  direction_angle_ = d;
+}
+float Robot::get_direction_angle_rad() {
+  return direction_angle_;
+}
+
 void Robot::MoveForward(int distance) {
   position_[0] = position_[0] + distance*cos(direction_angle_);
   position_[1] = position_[1] + distance*sin(direction_angle_);
