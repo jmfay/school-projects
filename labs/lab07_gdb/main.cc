6f8ba514ffd6ae8c8e002398f0ea22c37398eea6
fayxx092 <fayxx092@umn.edu>
2019-09-30 23:49:22 -0500

    add(Lab07): Adding Lab07 deliverables

--- labs/lab07_gdb/main.cc
+++ labs/lab07_gdb/main.cc
@@ -13,7 +13,7 @@ using namespace std;
 
 void doInterestingThing(int yr) {
   // variables used in this method
-  int* array_len;
+  int* array_len = new int;
   Date d1;
   Date d2;
   Date d3;
@@ -21,7 +21,7 @@ void doInterestingThing(int yr) {
 
   cout << " ... A set of Dates ... " << endl;
 
-  array_len = NULL;
+
   d2 = Date(yr, 4, 19);
   d3 = d2.copy();
   *array_len = 4;
@@ -41,16 +41,17 @@ void doInterestingThing(int yr) {
   }
 
   // now show the dates accumulated in reverse order
-  for (int i = 0; i < *array_len; --i) {
+  for (int i = (*array_len -1); i >= 0; --i) {
     cout << "date is: " << birthday_list[i]->show() << endl;
   }
 
   // now set the dates to January 2
-  for (int i = 0; i <= *array_len; i++) {
+  for (int i = 0; i < *array_len; i++) {
     *birthday_list[i] = Date(1990+i, 1, 2);
   }
-  
+
   cout << "the first date is: " << birthday_list[0]->show() << endl;
+  delete array_len;
 }
 
 int main() {
